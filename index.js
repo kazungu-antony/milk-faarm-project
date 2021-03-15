d7a09f8371a9d0af64fa96b5d7b29f73a9a5d411
kazungu-antony <antokazoga@gmail.com>
2021-03-15 08:44:06 +0300

    Created a function for displaying incomes

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ function totalProduction(){
     var totalMilk = shadeA + shadeB + shadeC + shadeD;
     
     document.getElementById("milkdisplay").innerHTML = totalMilk + " Liters " ;
+
+    document.getElementById("display-shade-a").innerHTML = "Shade A is "+ shadeA + " Liters " ;
+    document.getElementById("display-shade-b").innerHTML = "Shade A is "+ shadeB + " Liters " ;
+    document.getElementById("display-shade-c").innerHTML = "Shade A is "+ shadeC + " Liters " ;
+    document.getElementById("display-shade-d").innerHTML = "Shade A is "+ shadeD + " Liters " ;
+
 }
 
 
@@ -27,7 +33,15 @@ function totalProduction(){
 /* Finding incomes overtime */
 
 function incomeOverTime(){
-    var rate = parseInt(document.getElementById("milkrate").value);
+
+    var shadeA = parseInt(document.getElementById("totalmilkA").value);
+    var shadeB = parseInt(document.getElementById("totalmilkB").value);
+    var shadeC = parseInt(document.getElementById("totalmilkC").value);
+    var shadeD = parseInt(document.getElementById("totalmilkD").value);
+    var rate = parseInt(document.getElementById("milkrate").value);    
+    
+    var totalMilk = shadeA + shadeB + shadeC + shadeD;
+
 
     var day = 1;
     var week = 7;
@@ -35,10 +49,10 @@ function incomeOverTime(){
     var year = 365;
 
         
-        var dayilyIncome = day * rate;
-        var weeklyIncome = week * rate;
-        var monthlyIncome = month * rate;
-        var yearlyIncome = year * rate;
+        var dailyIncome = day * rate * totalMilk;
+        var weeklyIncome = week * rate * totalMilk;
+        var monthlyIncome = month * rate * totalMilk;
+        var yearlyIncome = year * rate * totalMilk;
 
     document.getElementById("display-2").innerHTML = dailyIncome + " KSH " ;
     document.getElementById("display-3").innerHTML = monthlyIncome + " KSH " ;
@@ -46,5 +60,7 @@ function incomeOverTime(){
     document.getElementById("display-5").innerHTML = yearlyIncome + " KSH " ;
 
 }
+
+
 
 
